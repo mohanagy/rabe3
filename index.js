77f6b058f8fccd68998f978ef876e8d79dfc130f
Mohammed Naji <mohammed.naji@al-majid.com>
2021-06-29 11:32:49 +0400

    fix

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const puppeteer = require("puppeteer");
+const ObjectsToCsv = require("objects-to-csv");
+const { Worker, isMainThread, parentPort } = require("worker_threads");
+
 const fs = require("fs");
 (async () => {
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-    });
-    const page = await browser.newPage();
-    await page.goto("https://vendor.gramms.org/", {
-      waitUntil: "networkidle2",
-    });
+    if (isMainThread) {
+      const browser = await puppeteer.launch({
+        headless: true,
+        timeout: 9999999,
+      });
+      const page = await browser.newPage();
+      await page.goto("https://vendor.gramms.org/", {
+        waitUntil: "networkidle2",
+      });
 
-    await page.waitFor("input[name=UserName]");
+      await page.waitFor("input[name=UserName]");
 
-    await page.type("input[name=UserName]", "Dlock.steel.co@gmail.com");
-    await page.type("input[name=Password]", "Zxcv123456?");
-    await page.$eval('button[type="submit"]', (form) => form.click());
+      await page.type("input[name=UserName]", "Dlock.steel.co@gmail.com");
+      await page.type("input[name=Password]", "Zxcv123456?");
+      await page.$eval('button[type="submit"]', (form) => form.click());
 
-    await page.waitForNavigation();
-    await page.screenshot({ path: "step1.png", fullPage: true });
-    await page.goto("https://vendor.gramms.org/", {
-      waitUntil: "networkidle2",
-    });
-    await page.screenshot({ path: "step2.png", fullPage: true });
+      await page.waitForNavigation();
+      await page.screenshot({ path: "step1.png", fullPage: true });
+      await page.goto("https://vendor.gramms.org/", {
+        waitUntil: "networkidle2",
+      });
+      await page.screenshot({ path: "step2.png", fullPage: true });
 
-    const file = await fs.readFileSync("./ids.txt", {
-      encoding: "utf8",
-      flag: "r",
-    });
-    const ids = file.split("\n").filter((value) => value);
-    const result = {};
+      const file = await fs.readFileSync("./ids.txt", {
+        encoding: "utf8",
+        flag: "r",
+      });
+      const ids = file.split("\n").filter((value) => value);
+    }
+    const result = [];
     for await (const id of ids) {
       await page.goto(
         "https://vendor.gramms.org/#/form/SearchForBeneficiary?BeneficiaryId=" +
@@ -45,32 +51,56 @@ const fs = require("fs");
         'span[translate-default="OriginalQuantity"]'
       );
       if (Inventory) {
-        result[id] = "Passed";
         let active = await page.$(
           'button[data-form="CreateBeneficiaryReceipt"]'
         );
 
-        if (active) result[id] = result[id] + " - Active";
-        else result[id] = result[id] + " - not Active";
+        if (active) {
+          let parent = await page.$$(
+            'json-report-value[data-name="ActivatedQuantity"]>span>table>tbody>tr'
+          );
+          const array = await Promise.all(
+            parent.map(async (element) => {
+              return await element.$$eval("td", (x) =>
+                x.map((n) => n.innerText)
+              );
+            })
+          );
+          const object = {
+            id,
+            status: "Passed - Active",
+            Rebar: 0,
+            Cement: 0,
+          };
+          object[array[0][1]] = array[0][2];
+          if (array[1]) {
+            object[array[1][1]] = array[1][2];
+          }
+          result.push(object);
+        } else {
+          result.push({
+            id,
+            status: "Passed - Not Active",
+            Cement: 0,
+            Rebar: 0,
+          });
+        }
       } else {
-        result[id] = "Failed";
+        result.push({
+          id,
+          status: "Failed",
+          Cement: 0,
+          Rebar: 0,
+        });
       }
     }
-    await fs.writeFileSync("./result.json", JSON.stringify(result));
-    // Specified beneficiary has no active projects in the system. For questions please contact MoCA.
-    // await page.screenshot({ path: "step4.png" });
-    // await page.waitFor('input[ng-model="c.input.manager.Value"]');
-    // await page.screenshot({ path: "step3.png" });
-    // await page.type('input[ng-model="c.input.manager.Value"]', 900178369);
-    // await page.$eval('button[type="submit"]', (form) => form.click());
+    const csv = new ObjectsToCsv(result);
 
-    //   await page.waitForSelector('#mw-content-text');
-
-    //   await page.$eval('input[name=search]', el => el.value = 'Adenosine triphosphate');
+    // Save to file:
+    await csv.toDisk("./result.csv");
 
     await browser.close();
   } catch (error) {
-    await browser.close();
     console.log({
       error,
     });
